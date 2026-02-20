5988880daed3e68e62d4664b358a58fcc650b71d
Lakshmidurga9000 <slakshmidurga96@gmail.com>
2026-02-20 22:44:55 +0530

    Add files via upload

---
+++ Shopez/backend/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/products", require("./routes/products"));
+app.use("/api/cart", require("./routes/cart"));
+app.use("/api/orders", require("./routes/orders"));
+
+app.listen(process.env.PORT, () =>
+  console.log("🚀 Backend running on port 5000")
+);
